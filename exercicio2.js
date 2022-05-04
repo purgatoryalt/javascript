e6f1b8f091736f9d93524467af7acaa20c9b54c6
purgatoryalt <102757720+purgatoryalt@users.noreply.github.com>
2022-05-04 16:48:53 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,17 @@
+//3
+const nome='Leonardo Cardoso';
+console.log(nome);
+//4
+const x=3;
+const y=5;
+
+console.log(x*y);
+//5
+const a=3;
+const b=4;
+const c=6;
+console.log(a+b+c)
+//6
+const s=1800;
+console.log(s*2);
+
